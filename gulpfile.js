86e7a45644318d98f7defaa2f251e2a8f6c9dc9d
Alloffices <alloffices.nyc@gmail.com>
2015-12-08 01:00:36 -0500

    node.js gulp autoprefixer

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+
+
+gulp.task('styles',function() {
+  gulp.src('css/styles.css')
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('build'))
+});
